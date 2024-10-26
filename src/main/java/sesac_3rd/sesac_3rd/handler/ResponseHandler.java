5bef3f61ccf7f7b2bd5233ee02bbeecbc8039522
qldirr <sub05073@naver.com>
2024-10-26 20:17:41 +0900

    add : security 관련 build와 설정 파일 추가, 응답구조 통일 파일 추가

--- src/main/java/sesac_3rd/sesac_3rd/handler/ResponseHandler.java
+++ src/main/java/sesac_3rd/sesac_3rd/handler/ResponseHandler.java
@@ -1,5 +1,15 @@
 package sesac_3rd.sesac_3rd.handler;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 // 커스텀 예외 제외한 응답 구조 통일
-public class ResponseHandler {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseHandler<T> {
+    private T data;
+    private int status;      // HTTP 상태 코드
+    private String message;
 }

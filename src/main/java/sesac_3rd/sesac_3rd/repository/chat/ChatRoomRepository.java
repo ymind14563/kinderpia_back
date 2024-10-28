bb42a112ebc21da69a63cf4673c4073221faaced
ymind14563 <ymind14563@gmail.com>
2024-10-29 02:58:27 +0900

    feat: findByUserId 추가

--- src/main/java/sesac_3rd/sesac_3rd/repository/chat/ChatRoomRepository.java
+++ src/main/java/sesac_3rd/sesac_3rd/repository/chat/ChatRoomRepository.java
@@ -1,7 +1,13 @@
 package sesac_3rd.sesac_3rd.repository.chat;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sesac_3rd.sesac_3rd.entity.ChatRoom;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("SELECT cr FROM ChatRoom cr JOIN Meeting m ON cr.meeting.meetingId = m.meetingId JOIN UserMeeting um ON um.meeting.meetingId = m.meetingId WHERE um.user.userId = :userId")
+    Page<ChatRoom> findByUserId(Long userId, Pageable pageable);
 }

fcf6b10fe98967cc82660a289f2cee91cfe137c6
errorose <yoonyeseul2000@gmail.com>
2024-10-28 20:32:47 +0900

    feat: meeting 모임상태 조회, 키워드 조회, 커스텀 에러 meeting 추가

--- src/main/java/sesac_3rd/sesac_3rd/controller/MeetingController.java
+++ src/main/java/sesac_3rd/sesac_3rd/controller/MeetingController.java
@@ -3,6 +3,7 @@ package sesac_3rd.sesac_3rd.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sesac_3rd.sesac_3rd.dto.meeting.MeetingDTO;
 import sesac_3rd.sesac_3rd.service.meeting.MeetingService;
@@ -15,8 +16,22 @@ public class MeetingController {
     @Autowired
     private MeetingService meetingService;
 
+    // 모임 목록 (default - 최신순 정렬)
     @GetMapping("/list")
     public List<MeetingDTO> getMeetingList() {
         return meetingService.getAllMeetings();
     }
+
+    // 모임 목록 (open - 열려있는것만 정렬)
+    @GetMapping("/list/open")
+    public List<MeetingDTO> getOpenMeetingList() {
+        return meetingService.getOpenMeetings();
+    }
+
+    // 키워드로 모임 검색
+    @GetMapping("/search")
+    public List<MeetingDTO> searchMeetings(@RequestParam String keyword) {
+        return meetingService.searchMeetingsByKeyword(keyword);
+    }
 }
+
--- src/main/java/sesac_3rd/sesac_3rd/service/meeting/MeetingServiceImpl.java
+++ src/main/java/sesac_3rd/sesac_3rd/service/meeting/MeetingServiceImpl.java
@@ -3,8 +3,11 @@ package sesac_3rd.sesac_3rd.service.meeting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import sesac_3rd.sesac_3rd.constant.MeetingStatus;
 import sesac_3rd.sesac_3rd.dto.meeting.MeetingDTO;
 import sesac_3rd.sesac_3rd.entity.Meeting;
+import sesac_3rd.sesac_3rd.exception.CustomException;
+import sesac_3rd.sesac_3rd.exception.ExceptionStatus;
 import sesac_3rd.sesac_3rd.repository.MeetingRepository;
 
 import java.util.ArrayList;
@@ -19,6 +22,69 @@ public class MeetingServiceImpl implements MeetingService {
     @Override
     public List<MeetingDTO> getAllMeetings() {
         List<Meeting> meetings = meetingRepository.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
+
+        if (meetings.isEmpty()) {
+            throw new CustomException(ExceptionStatus.MEETING_NOT_FOUND);
+        }
+
+        List<MeetingDTO> meetingDTOS = new ArrayList<>();
+
+        for (Meeting meeting : meetings) {
+            MeetingDTO meetingDTO = MeetingDTO.builder()
+                    .meetingId(meeting.getMeetingId())
+                    .placeId(meeting.getPlace().getPlaceId())
+                    .meetingCategory(meeting.getMeetingCategory().getMeetingCtgId()) // 카테고리
+                    .meetingTitle(meeting.getMeetingTitle()) // 타이틀
+                    .capacity(meeting.getCapacity()) // 참가인원
+                    .meetingLocation(meeting.getMeetingLocation()) // 모임장소
+                    .meetingTime(meeting.getMeetingTime()) // 모임일시
+                    .meetingStatus(meeting.getMeetingStatus()) // 모임 상태
+                    .createdAt(meeting.getCreatedAt()) // 생성일자
+                    .build();
+
+            meetingDTOS.add(meetingDTO);
+        }
+        return meetingDTOS;
+    }
+
+    // 모임 목록 (open - 열려있는것만 정렬)
+    @Override
+    public List<MeetingDTO> getOpenMeetings() {
+        List<Meeting> meetings = meetingRepository.findByMeetingStatus(MeetingStatus.ONGOING, Sort.by(Sort.Direction.DESC, "createdAt"));
+
+        if (meetings.isEmpty()) {
+            throw new CustomException(ExceptionStatus.MEETING_NOT_FOUND);
+        }
+
+        List<MeetingDTO> meetingDTOS = new ArrayList<>();
+
+        for (Meeting meeting : meetings) {
+            MeetingDTO meetingDTO = MeetingDTO.builder()
+                    .meetingId(meeting.getMeetingId())
+                    .placeId(meeting.getPlace().getPlaceId())
+                    .meetingCategory(meeting.getMeetingCategory().getMeetingCtgId()) // 카테고리
+                    .meetingTitle(meeting.getMeetingTitle()) // 타이틀
+                    .capacity(meeting.getCapacity()) // 참가인원
+                    .meetingLocation(meeting.getMeetingLocation()) // 모임장소
+                    .meetingTime(meeting.getMeetingTime()) // 모임일시
+                    .meetingStatus(meeting.getMeetingStatus()) // 모임 상태
+                    .createdAt(meeting.getCreatedAt()) // 생성일자
+                    .build();
+
+            meetingDTOS.add(meetingDTO);
+        }
+        return meetingDTOS;
+    }
+
+    // 키워드로 타이틀과 장소 검색
+    @Override
+    public List<MeetingDTO> searchMeetingsByKeyword(String keyword) {
+        List<Meeting> meetings = meetingRepository.findByMeetingTitleContainingOrMeetingLocationContaining(
+                keyword, keyword, Sort.by(Sort.Direction.DESC, "createdAt"));
+
+        if (meetings.isEmpty()) {
+            throw new CustomException(ExceptionStatus.MEETING_NOT_FOUND);
+        }
 
         List<MeetingDTO> meetingDTOS = new ArrayList<>();
 

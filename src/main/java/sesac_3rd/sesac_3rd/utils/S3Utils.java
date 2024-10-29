5420b6451096625b9e68b469d8fe7092f9a69964
qldirr <sub05073@naver.com>
2024-10-29 18:16:04 +0900

    add : s3 관련 dependency 추가 및 config 추가

--- src/main/java/sesac_3rd/sesac_3rd/utils/S3Utils.java
+++ src/main/java/sesac_3rd/sesac_3rd/utils/S3Utils.java
@@ -1,4 +1,11 @@
 package sesac_3rd.sesac_3rd.utils;
 
 public class S3Utils {
+//    public static String generatePresignedUrl(AmazonS3 s3Client, String bucket, String key) {
+//        // 미리 서명된 URL 생성 로직
+//    }
+//
+//    public static String extractFileKeyFromUrl(String s3Url) {
+//        // S3 URL에서 파일 키 추출 로직
+//    }
 }

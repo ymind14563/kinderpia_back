95eca5aafe5734e63a780f0e9c770aa7facdea10
ymind14563 <ymind14563@gmail.com>
2024-10-30 03:21:41 +0900

    feat: 채팅방 생성

--- src/main/java/sesac_3rd/sesac_3rd/service/chat/ChatRoomService.java
+++ src/main/java/sesac_3rd/sesac_3rd/service/chat/ChatRoomService.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import sesac_3rd.sesac_3rd.dto.chat.ChatRoomDTO;
 import sesac_3rd.sesac_3rd.entity.ChatRoom;
+import sesac_3rd.sesac_3rd.entity.Meeting;
+import sesac_3rd.sesac_3rd.exception.CustomException;
+import sesac_3rd.sesac_3rd.exception.ExceptionStatus;
 import sesac_3rd.sesac_3rd.handler.pagination.PaginationResponseDTO;
 import sesac_3rd.sesac_3rd.mapper.chat.ChatRoomMapper;
+import sesac_3rd.sesac_3rd.repository.MeetingRepository;
 import sesac_3rd.sesac_3rd.repository.chat.ChatRoomRepository;
 
 @Service
@@ -15,8 +19,11 @@ import sesac_3rd.sesac_3rd.repository.chat.ChatRoomRepository;
 public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatRoomMapper chatRoomMapper;
+    private final MeetingRepository meetingRepository;
 
 
+
+    // 유저 소속 채팅방 목록 조회
     public PaginationResponseDTO<ChatRoomDTO.ChatRoomList> getChatRooms(Long userId, int page, int size) {
         PageRequest pageRequest = PageRequest.of(page - 1, size);
         Page<ChatRoom> chatPage = chatRoomRepository.findByUserId(userId, pageRequest);
@@ -31,5 +38,23 @@ public class ChatRoomService {
 
         return new PaginationResponseDTO<>(chatRoomList, chatPage);
 
+    }
+
+    // 채팅방 생성
+    public void createChatRoomIfNotExists(Long meetingId) {
+
+        // Meeting 의 ChatRoom 존재 여부 확인
+        boolean chatRoomExists = chatRoomRepository.existsByMeeting_MeetingId(meetingId);
+
+        if (!chatRoomExists) {
+            Meeting meeting = meetingRepository.findById(meetingId)
+                    .orElseThrow(() -> new CustomException(ExceptionStatus.MEETING_NOT_FOUND));
+
+            ChatRoom chatRoom = new ChatRoom();
+            chatRoom.setMeeting(meeting);
+            chatRoom.setIsActive(true);
+
+            chatRoomRepository.save(chatRoom);
+        }
     }
 }

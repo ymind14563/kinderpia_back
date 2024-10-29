d5fe676813f3fd4fff548e66f7b561dde16edccb
yjyoo6831 <cenabro@naver.com>
2024-10-29 16:19:36 +0900

    feat: 리뷰 service, mapper 작성

---
+++ src/main/java/sesac_3rd/sesac_3rd/service/review/ReviewServiceImpl.java
@@ -0,0 +1,58 @@
+package sesac_3rd.sesac_3rd.service.review;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sesac_3rd.sesac_3rd.dto.review.ReviewDTO;
+import sesac_3rd.sesac_3rd.entity.Review;
+import sesac_3rd.sesac_3rd.exception.CustomException;
+import sesac_3rd.sesac_3rd.exception.ExceptionStatus;
+import sesac_3rd.sesac_3rd.repository.ReviewRepository;
+
+import static sesac_3rd.sesac_3rd.mapper.review.ReviewMapper.convertToDTO;
+import static sesac_3rd.sesac_3rd.mapper.review.ReviewMapper.convertToEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class ReviewServiceImpl implements ReviewService{
+    // 메서드 구현
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    // 리뷰 목록 조회
+    @Override
+    public List<ReviewDTO> getAllReview(Long placeId){
+            List<Review> review = reviewRepository.findAllById(placeId);
+            List<ReviewDTO> reviewDTO = new ArrayList<>();
+            for(Review r : review){
+                ReviewDTO dto = convertToDTO(r);
+                reviewDTO.add(dto);
+            }
+            return reviewDTO;
+    }
+
+    // 리뷰 단건 조회
+    @Override
+    public ReviewDTO getReviewById(Long reviewId){
+        Review review = reviewRepository.findById(reviewId).orElseThrow(()->new CustomException(ExceptionStatus.REVIEWID_NOT_FOUND));
+        return convertToDTO(review);
+    }
+
+    // 리뷰 작성
+    @Override
+    public void createReview(ReviewDTO reviewDTO) {
+        Review review = convertToEntity(reviewDTO);
+        reviewRepository.save(review);
+    }
+
+
+
+    // 리뷰 수정
+
+    // 리뷰 삭제
+
+
+}

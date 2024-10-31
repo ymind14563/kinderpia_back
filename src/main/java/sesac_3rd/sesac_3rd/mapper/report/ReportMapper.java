d0cba04fb69bc2d9d382edf0b845401141f9d1d7
ymind14563 <ymind14563@gmail.com>
2024-11-01 05:30:31 +0900

    feat: report DTO, Mapper

--- src/main/java/sesac_3rd/sesac_3rd/mapper/report/ReportMapper.java
+++ src/main/java/sesac_3rd/sesac_3rd/mapper/report/ReportMapper.java
@@ -1,4 +1,52 @@
 package sesac_3rd.sesac_3rd.mapper.report;
 
+import org.springframework.stereotype.Component;
+import sesac_3rd.sesac_3rd.dto.report.ReportDTO;
+import sesac_3rd.sesac_3rd.entity.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class ReportMapper {
+
+    public ReportDTO.Report reportToReportResponseDto(Report report) {
+        return ReportDTO.Report.builder()
+                .reportId(report.getReportId())
+                .chatMessageId(report.getChatMessage() != null ? report.getChatMessage().getSender().getUserId() : null)
+                .reviewId(report.getReview() != null ? report.getReview().getUser().getUserId() : null)
+                .meetingId(report.getMeeting() != null ? report.getMeeting().getMeetingId() : null)
+                .reporterId(report.getReporter().getUserId())
+                .reportedId(report.getReported().getUserId())
+                .reportReasonId(report.getReportReason().getReportRsId())
+                .reportReasonName(report.getReportReason().getReportRsName())
+                .reportMessageContent(report.getReportMessageContent())
+                .createdAt(report.getCreatedAt())
+                .build();
+    }
+
+    public ReportDTO.ReportList reportListToReportListResponseDto(List<Report> reports) {
+        List<ReportDTO.Report> reportList = reports.stream()
+                .map(this::reportToReportResponseDto)
+                .collect(Collectors.toList());
+        return ReportDTO.ReportList.builder()
+                .reportList(reportList)
+                .build();
+    }
+
+    public Report reportDTOtoEntity(ReportDTO.Report reportDto, User reporter, User reported,
+                                 ChatMessage chatMessage, Review review, Meeting meeting, ReportReason reportReason) {
+        Report report = new Report();
+        report.setReporter(reporter);
+        report.setReported(reported);
+        report.setChatMessage(chatMessage);
+        report.setReview(review);
+        report.setMeeting(meeting);
+        report.setReportReason(reportReason);
+        report.setReportMessageContent(reportDto.getReportMessageContent());
+        report.setCreatedAt(LocalDateTime.now());
+        return report;
+    }
+
 }

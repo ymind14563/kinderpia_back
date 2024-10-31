1fdfa8847ab2eb1ba569a3282a28563368efb406
yjyoo6831 <cenabro@naver.com>
2024-10-31 15:52:32 +0900

    fix: 리뷰 ì¡목록 조회

--- src/main/java/sesac_3rd/sesac_3rd/controller/PlaceController.java
+++ src/main/java/sesac_3rd/sesac_3rd/controller/PlaceController.java
@@ -2,9 +2,16 @@ package sesac_3rd.sesac_3rd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sesac_3rd.sesac_3rd.dto.meeting.MeetingDTO;
 import sesac_3rd.sesac_3rd.dto.place.PlaceDTO;
 import sesac_3rd.sesac_3rd.entity.Place;
+import sesac_3rd.sesac_3rd.handler.ResponseHandler;
+import sesac_3rd.sesac_3rd.handler.pagination.PaginationResponseDTO;
 import sesac_3rd.sesac_3rd.service.place.PlaceService;
 
 import java.util.List;
@@ -21,11 +28,18 @@ public class PlaceController {
     private Page<Place> getAllPlace(
             @RequestParam(defaultValue = "date") String sort, //정렬 순서
             @RequestParam(defaultValue = "0") int page, //시작 페이지
-            @RequestParam(defaultValue = "10") int limit, //크기
+            @RequestParam(defaultValue = "10") int size, //크기
             @RequestParam(defaultValue = "all") String category, //검색 카테고리
-            @RequestParam(defaultValue = "none") String keyword
-    ) { // 검색 키워드
-        return placeService.getAllPlace(sort, page, limit, category, keyword);
+            @RequestParam(defaultValue = "none") String keyword // 검색 키워드
+    ) {
+        try{
+            Pageable pageable = PageRequest.of(page-1, size);
+            return placeService.getAllPlace(sort, page, size , category, keyword);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     // 장소 검색
@@ -42,11 +56,20 @@ public class PlaceController {
 
     // 장소 상세 조회
     @GetMapping("/{id}")
-    private PlaceDTO getPlaceById(@PathVariable long placeId){
-        return placeService.getPlaceById(placeId);
-    }
+    private ResponseEntity<ResponseHandler<PlaceDTO>> getPlaceById(@PathVariable("id") Long placeId){
+        try {
+            PlaceDTO placeDTO= placeService.getPlaceById(placeId);
+            ResponseHandler<PlaceDTO> response = new ResponseHandler<>(
+                    placeDTO,
+                    HttpStatus.OK.value(),
+                    "장소 상세 조회 완료"
+            );
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-    // 장소 검색
+    }
 
 
 }

d2e81bdf4eb8f823ef70d14a95a2d0f6037bda30
ymind14563 <ymind14563@gmail.com>
2024-11-02 01:09:01 +0900

    fix: PaginationResponseDTO null 포함되지 않도록 수정

--- src/main/java/sesac_3rd/sesac_3rd/handler/pagination/PaginationResponseDTO.java
+++ src/main/java/sesac_3rd/sesac_3rd/handler/pagination/PaginationResponseDTO.java
@@ -3,12 +3,21 @@ package sesac_3rd.sesac_3rd.handler.pagination;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 
 import java.util.List;
 
 @Getter
 public class PaginationResponseDTO<T> {
+    // 현재 오버로딩하더라도 모든 필드(dataList, data, pageInfo)는 객체가 생성될 때 포함.
+    // PaginationResponseDTO 가 List 나 data 방식 하나만 쓰더라도 나머지 하나가 null 로 표시 되는 문제
+    // @JsonInclude(JsonInclude.Include.NON_NULL) 사용으로 null 포함되지 않도록 함
+
+    @JsonInclude(JsonInclude.Include.NON_NULL) // null 포함되지 않도록 함
     private List<T> dataList;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
     private PageInfo pageInfo;
 

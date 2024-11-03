2c5b2844827396ee87169fb3f1847bffacaf4949
ymind14563 <ymind14563@gmail.com>
2024-11-03 18:40:48 +0900

    fix: null 로 기본값 지정

--- src/main/java/sesac_3rd/sesac_3rd/mapper/chat/ChatMessageMapper.java
+++ src/main/java/sesac_3rd/sesac_3rd/mapper/chat/ChatMessageMapper.java
@@ -30,12 +30,26 @@ public class ChatMessageMapper {
 
     // 채팅방 메세지 단일 조회 (리스트용)
     public static ChatMessageDTO.ChatMessage ChatMessageToChatMessageResponseDTO(ChatMessage chatMessage) {
+        // 문제: chatMessage.setSender(null); 산입으로 인해
+        // sender 필드가 null 인 경우, chatMessage.getSender() 호출 시 NullPointerException이 발생할 수 있음
+        // 해결방법: sender가 null인 경우 각 필드에 대해 적절한 기본값(null 또는 "System")을 설정하여
+        // NullPointerException 발생을 방지
+
+        // 기본값으로 null을 명시적으로 주는 것 (의도) 과 null인 상태 그대로 두는 것 (누락으로 판단) 은 차이가 있음
+        // null 을 명시적으로 설정하면 의도적 비움으로 간주하여 예상 동작 유도 가능
+        // 그대로 두면 NullPointerException 발생 위험 있으므로 기본값 설정으로 예외 방지 필요
+
+        Long senderId = chatMessage.getSender() != null ? chatMessage.getSender().getUserId() : null;
+        String senderNickname = chatMessage.getSender() != null ? chatMessage.getSender().getNickname() : "System";
+        String senderProfileImg = chatMessage.getSender() != null ? chatMessage.getSender().getProfileImg() : null;
+
+
         return ChatMessageDTO.ChatMessage.builder()
                 .chatmsgId(chatMessage.getChatmsgId())
                 .chatroomId(chatMessage.getChatRoom().getChatroomId())
-                .senderId(chatMessage.getSender().getUserId())
-                .senderNickname(chatMessage.getSender().getNickname())
-                .senderProfileImg(chatMessage.getSender().getProfileImg())
+                .senderId(senderId)
+                .senderNickname(senderNickname)
+                .senderProfileImg(senderProfileImg)
                 .chatmsgContent(chatMessage.getChatmsgContent())
                 .messageType(chatMessage.getMessageType())
                 .createdAt(chatMessage.getCreatedAt())

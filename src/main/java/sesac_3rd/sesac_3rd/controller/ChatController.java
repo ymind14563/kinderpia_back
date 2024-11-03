e19b0246239dc0aa257ac7a82bc59a2853aaf4ae
ymind14563 <ymind14563@gmail.com>
2024-11-03 18:40:18 +0900

    fix: JWT 적용, get에서 post 방식으로 변경

--- src/main/java/sesac_3rd/sesac_3rd/controller/ChatController.java
+++ src/main/java/sesac_3rd/sesac_3rd/controller/ChatController.java
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sesac_3rd.sesac_3rd.dto.chat.ChatMessageDTO;
 import sesac_3rd.sesac_3rd.dto.chat.ChatRoomDTO;
+import sesac_3rd.sesac_3rd.dto.chat.ChatRoomRequestDTO;
 import sesac_3rd.sesac_3rd.handler.ResponseHandler;
 import sesac_3rd.sesac_3rd.handler.pagination.PaginationResponseDTO;
 import sesac_3rd.sesac_3rd.mapper.chat.ChatMessageMapper;
@@ -30,13 +32,20 @@ public class ChatController {
 //    private final SimpMessagingTemplate messagingTemplate;
 
     // 전체 채팅 목록 조회 (페이지네이션 적용)
-    // /api/chatroom/list/{userId}?page={page}&size={size}
-    @GetMapping("/list/{userId}")
+    // /api/chatroom/list?page={page}&size={size}
+    @PostMapping("/list")
     public ResponseEntity<ResponseHandler<PaginationResponseDTO<ChatRoomDTO.ChatRoomList>>> getChatRoomList(
-            @Positive @PathVariable("userId") Long userId,
+            @AuthenticationPrincipal Long userId,
             @RequestParam(value = "page", required = false, defaultValue = "1") int page,
-            @RequestParam(value = "size", required = false, defaultValue = "10") int size
-    ) {
+            @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+
+//        System.out.println("UserId" + userId);
+
+        // 토큰에 문제가 있는 경우
+        if (userId == null) {
+            return ResponseHandler.unauthorizedResponse();
+        }
+
         PaginationResponseDTO<ChatRoomDTO.ChatRoomList> chatRooms = chatRoomService.getChatRooms(userId, page, size);
 
         return ResponseHandler.response(chatRooms, HttpStatus.OK, "채팅방 목록 조회 성공");
@@ -45,10 +54,16 @@ public class ChatController {
 
 
     // 단일 채팅방 조회
-    // /api/chatroom/{chatroomId}/{userId}
-    @PostMapping("/{chatroomId}")
-    public ResponseEntity<ResponseHandler<ChatRoomDTO.ChatRoom>> getChatRoomById(@Positive @RequestBody Long chatroomId) {
-        ChatRoomDTO.ChatRoom chatRoom = chatRoomService.getChatRoomById(chatroomId);
+    @PostMapping
+    public ResponseEntity<ResponseHandler<ChatRoomDTO.ChatRoom>> getChatRoomById(@AuthenticationPrincipal Long userId,
+                                                                                 @RequestBody ChatRoomRequestDTO chatRoomRequestDTO) {
+
+        // 토큰에 문제가 있는 경우
+        if (userId == null) {
+            return ResponseHandler.unauthorizedResponse();
+        }
+        ChatRoomDTO.ChatRoom chatRoom = chatRoomService.getChatRoomById(userId, chatRoomRequestDTO.getChatroomId());
+
         return ResponseHandler.response(chatRoom, HttpStatus.OK, "채팅방 조회 성공");
     }
 
@@ -57,7 +72,8 @@ public class ChatController {
     // 메시지 저장 및 전송
     @MessageMapping("/{chatroomId}/chatmsg")
     @SendTo("/topic/chatroom/{chatroomId}")
-    public ResponseEntity<ResponseHandler<ChatMessageDTO.ChatMessage>> sendMessage(@DestinationVariable Long chatroomId, ChatMessageDTO.ChatMessage chatMessageDTO) {
+    public ResponseEntity<ResponseHandler<ChatMessageDTO.ChatMessage>> sendMessage(@DestinationVariable Long chatroomId,
+                                                                                   ChatMessageDTO.ChatMessage chatMessageDTO) {
 
         ChatMessageDTO.ChatMessage savedMessage = chatMessageService.saveMessage(chatroomId, chatMessageDTO);
 
@@ -68,13 +84,21 @@ public class ChatController {
 
 
     // 메세지 내역 조회 (페이지네이션 적용 - 채팅생성시간 오름차순 적용)
-    @GetMapping("/chatmsg/{chatroomId}")
+    @PostMapping("/chatmsg")
     // /api/chatroom/chatmsg/{chatroomId}?page={page}&size={size}
-    public ResponseEntity<ResponseHandler<PaginationResponseDTO<ChatMessageDTO.ChatMessageList>>> getChatMessages(@PathVariable("chatroomId") Long chatroomId,
-                                                                                                                  @RequestParam int page,
-                                                                                                                  @RequestParam int size) {
+    public ResponseEntity<ResponseHandler<PaginationResponseDTO<ChatMessageDTO.ChatMessageList>>> getChatMessages(
+            @AuthenticationPrincipal Long userId,
+            @RequestBody ChatRoomRequestDTO chatRoomRequestDTO,
+            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
 
-        PaginationResponseDTO<ChatMessageDTO.ChatMessageList> chatMessages = chatMessageService.getChatMessages(chatroomId, page, size);
+        if (userId == null) {
+            return ResponseHandler.unauthorizedResponse();
+        }
+
+        System.out.println(userId);
+        System.out.println(chatRoomRequestDTO.getChatroomId());
+        PaginationResponseDTO<ChatMessageDTO.ChatMessageList> chatMessages = chatMessageService.getChatMessages(userId, chatRoomRequestDTO.getChatroomId(), page, size);
 
         return ResponseHandler.response(chatMessages, HttpStatus.OK, "채팅 메시지 리스트 조회 성공");
     }

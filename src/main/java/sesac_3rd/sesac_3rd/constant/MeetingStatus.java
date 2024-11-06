0a3e8af3a5cece0253a16a51b089dc58e1c36e2a
errorose <yoonyeseul2000@gmail.com>
2024-11-06 20:40:29 +0900

    feat: meeting 모집완료 추가

--- src/main/java/sesac_3rd/sesac_3rd/constant/MeetingStatus.java
+++ src/main/java/sesac_3rd/sesac_3rd/constant/MeetingStatus.java
@@ -3,7 +3,7 @@ package sesac_3rd.sesac_3rd.constant;
 // 모임상태
 public enum MeetingStatus {
     ONGOING,   // 모집중
-    COMPLETED,   // 인원마감
-    END,   // 모임종료
-    DELETED   // 모임삭제
+    COMPLETED,   // 모집완료 (인원마감, 모임장이 임의로 마감)
+    END,   // 모임종료 (시간이 지나서 마감)
+    DELETED   // 모임삭제 (모임장이 삭제, 관리자가 삭제)
 }

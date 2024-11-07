6339167c0847a20b359cf05631a1d7002c3f5d28
qldirr <sub05073@naver.com>
2024-11-07 19:20:04 +0900

    fix : 관리자 로그인 리턴 변경 및 react 빌드 경로 추가

---
+++ src/main/java/sesac_3rd/sesac_3rd/controller/ManagerViewController.java
@@ -0,0 +1,12 @@
+package sesac_3rd.sesac_3rd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ManagerViewController {
+    @GetMapping({"/admin", "/admin/**"})
+    public String serveReactApp(){
+        return "forward:/index.html";
+    }
+}

0989424ea79b6ff796d9d6885dcd6917dd3b7ae2
qldirr <sub05073@naver.com>
2024-11-07 19:31:40 +0900

    fix : 관리자 로그인 리턴 변경

--- src/main/java/sesac_3rd/sesac_3rd/controller/ManagerViewController.java
+++ src/main/java/sesac_3rd/sesac_3rd/controller/ManagerViewController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ManagerViewController {
     @GetMapping({"/admin", "/admin/**"})
     public String serveReactApp(){
-        return "forward:/index.html";
+        return "forward:/index2.html";
     }
 }

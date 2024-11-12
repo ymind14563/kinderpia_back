50a6171539536f08eda8094f87a32e70ec820407
ymind14563 <ymind14563@gmail.com>
2024-11-13 04:20:05 +0900

    fix: chat notification

--- src/main/java/sesac_3rd/sesac_3rd/repository/chat/ChatRoomStatusRepository.java
+++ src/main/java/sesac_3rd/sesac_3rd/repository/chat/ChatRoomStatusRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ChatRoomStatusRepository {
     // Redis에서 Pub/Sub 기능을 사용하거나 Redis 템플릿을 직접 조작해야 한다면, 클래스를 사용하여 Redis 작업을 처리하는 것이 일반적
+
     private final RedisTemplate<String, Object> redisTemplate;
     private final HashOperations<String, Long, Boolean> hashOpsEnterInfo;
 
@@ -15,18 +16,21 @@ public class ChatRoomStatusRepository {
         this.hashOpsEnterInfo = redisTemplate.opsForHash();
     }
 
-//    public void enterChatRoom(Long chatroomId, Long userId) {
-//        String key = chatroomId.toString();
-//        hashOpsEnterInfo.put(key, userId, true);
-//    }
-//
-//    public void exitChatRoom(Long chatroomId, Long userId) {
-//        String key = chatroomId.toString();
-//        hashOpsEnterInfo.delete(key, userId);
-//    }
+    public void enterChatRoom(Long chatroomId, Long userId) {
+        String key = chatroomId.toString();
+        hashOpsEnterInfo.put(key, userId, true);
+        System.out.println("사용자 " + userId + "님 채팅방에 입장, 번호: " + chatroomId);
+    }
+
+    public void exitChatRoom(Long chatroomId, Long userId) {
+        String key = chatroomId.toString();
+        hashOpsEnterInfo.delete(key, userId);
+        System.out.println("사용자 " + userId + "님 채팅방에 퇴장, 번호: " + chatroomId);
+    }
 
     public boolean isUserInChatRoom(Long chatroomId, Long userId) {
         String key = chatroomId.toString(); // redis 는 문자열 기반
         return hashOpsEnterInfo.hasKey(key, userId);
+
     }
 }

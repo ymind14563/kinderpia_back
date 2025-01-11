829da601cb19d3d346fe34ad17b32495d4d7e64d
ymind14563 <ymind14563@gmail.com>
2025-01-11 14:01:09 +0900

    feat: 인기 장소 조회 쿼리 추가

--- src/main/java/sesac_3rd/sesac_3rd/repository/PlaceRepository.java
+++ src/main/java/sesac_3rd/sesac_3rd/repository/PlaceRepository.java
@@ -8,14 +8,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sesac_3rd.sesac_3rd.dto.place.PlaceReviewDTO;
+import sesac_3rd.sesac_3rd.dto.place.PopularPlaceDTO;
 import sesac_3rd.sesac_3rd.entity.Place;
+
+import java.util.List;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
     // 장소 아이디로 조회
     Place findByPlaceId(Long placeId);
 
-    // 전체 조회 (이 부분은 올바르게 작성되어 있습니다)
+    // 전체 조회
     @Query("SELECT new sesac_3rd.sesac_3rd.dto.place.PlaceReviewDTO(" +
             "p.placeId, p.placeName, p.location, " +
             "p.detailAddress, p.operatingDate, p.latitude, p.longitude, p.placeImg, p.homepageUrl, p.placeNum, p.isPaid, pc.placeCtgName, " +
@@ -52,4 +55,20 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
             "WHERE p.placeName LIKE %:keyword% " +
             "GROUP BY p.placeId, p.placeName, p.location, p.detailAddress, p.operatingDate, p.latitude, p.longitude, p.placeImg, p.homepageUrl, p.placeNum, p.isPaid, pc.placeCtgName")
     Page<PlaceReviewDTO> findByPlaceNameContaining(@Param("keyword") String keyword, Pageable pageable);
+
+
+    // 인기 장소 (상위 8개)
+    @Query("SELECT new sesac_3rd.sesac_3rd.dto.place.PopularPlaceDTO(" +
+            "p.placeId, p.placeName, p.placeImg, p.isPaid, pc.placeCtgName, " +
+            "CAST(COALESCE(ROUND(AVG(CASE WHEN r.isDeleted = false THEN r.star ELSE null END)), 0) AS int) AS averageStar, " +
+            "CAST(COUNT(CASE WHEN r.isDeleted = false THEN r.id ELSE null END) AS int) AS totalReviewCount) " +
+            "FROM Place p " +
+            "JOIN p.placeCategory pc " +
+            "LEFT JOIN Review r ON p.placeId = r.place.placeId " +
+            "GROUP BY p.placeId, p.placeName, p.placeImg, p.isPaid, pc.placeCtgName " +
+            "ORDER BY averageStar DESC")
+    List<PopularPlaceDTO> getTop8PopularPlaces();
+
 }
+
+

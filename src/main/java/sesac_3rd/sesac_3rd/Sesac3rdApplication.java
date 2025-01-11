f32c7a3b7ff75acd5df3e98b832a6e1f9c720db8
ymind14563 <ymind14563@gmail.com>
2025-01-11 14:08:57 +0900

    feat: 인기 장소 갱신 Scheduling

--- src/main/java/sesac_3rd/sesac_3rd/Sesac3rdApplication.java
+++ src/main/java/sesac_3rd/sesac_3rd/Sesac3rdApplication.java
@@ -3,9 +3,11 @@ package sesac_3rd.sesac_3rd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class Sesac3rdApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/sesac_3rd/sesac_3rd/scheduler/PopularPlaceScheduler.java
@@ -0,0 +1,27 @@
+package sesac_3rd.sesac_3rd.scheduler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import sesac_3rd.sesac_3rd.service.place.PlaceService;
+
+@Service
+public class PopularPlaceScheduler {
+
+    @Autowired
+    private PlaceService placeService;
+
+    // 매일 새벽 1시에 실행 (cron: 초, 분, 시, 일, 월, 요일)
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void updatePopularPlaces() {
+        try {
+            System.out.println("인기 장소 갱신 시작");
+
+            // DB에서 인기 장소를 가져와 Redis에 저장
+            placeService.savePopularPlacesToRedis(placeService.getPopularPlaces());
+            System.out.println("인기 장소 갱신 완료");
+        } catch (Exception e) {
+            System.err.println("인기 장소 갱신 중 오류 발생: " + e.getMessage());
+        }
+    }
+}

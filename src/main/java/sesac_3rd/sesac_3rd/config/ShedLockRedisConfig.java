518d181ac65c539a715f963e391103229332ef06
ymind14563 <ymind14563@gmail.com>
2025-02-18 23:06:12 +0900

    comment: ShedLock

--- src/main/java/sesac_3rd/sesac_3rd/config/ShedLockRedisConfig.java
+++ src/main/java/sesac_3rd/sesac_3rd/config/ShedLockRedisConfig.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 public class ShedLockRedisConfig {
-
+//    여러 서버에서 동일한 @Scheduled 작업이 동시에 실행되는 것을 방지
     @Bean
     public LockProvider lockProvider(RedisConnectionFactory redisConnectionFactory) {
         return new RedisLockProvider(redisConnectionFactory);
